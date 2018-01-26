aceff85717901445352a5d48fde4b973943c0936
Nathan Greenstein <nathang345@gmail.com>
2018-01-26 13:38:38 -0500

    More cleanup

--- moth_radio/views.py
+++ moth_radio/views.py
@@ -7,21 +7,3 @@ from models import LabUser, Stimulus, Session, Rating
 def start_exp():
 	stimuli = apis.fetchStimuli(count = app.config["num_stim"], modality = "video")
 	return render_template('exp_moth_loop.html', stimuli = stimuli, stimBase = app.config["stim_base"], num_stops = app.config["num_stops"])
-
-@app.route('/ratings', methods =['POST'])
-def rating_to_db():
-	#ratings= Post.from_json(request.json) 
-	print 'received rating!'
-	print(request.data)
-	content = request.get_json(silent=True)
-	print content
-	
-	ratingRecords = content['emotionSelections']
-	ratingObjs = []
-	for emotionName in ratingRecords: # for each dictionary in the list
-		# ratingObj = Ratings(category=emotionName, intensity=ratingRecords[emotionName], selected=True, reaction_time=content['rt'])
-		# ratingObjs.append(ratingObj)
-		pass
-	db.session.add_all(ratingObjs)
-	db.session.commit()
-	return json.dumps(content)
